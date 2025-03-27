05854c33dd0c6163987d34325b1f311ec2add1be
Ampoy <98205081+ampoy19@users.noreply.github.com>
2025-03-27 15:54:50 +0800

    Add files via upload

---
+++ ios-speedometer-main/SimulationApp/SimulationApp/SimulationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulationAppApp.swift
+//  SimulationApp
+//
+//  Created by Allan Caraig on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SimulationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
